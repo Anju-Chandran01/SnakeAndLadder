720ca8543b973667cc3d17178d48141689e527c9
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-03-06 18:54:55 +0530

    Added : Checks for option Ladder, Snake or Not play

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,10 +1,31 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println(" --- Snake and Ladder Problem --- ");
 
-        int startPosition = 0;
-        System.out.println("Game starts from : " + startPosition);
+        int playerPosition = 0;
+        System.out.println("Game starts from : " + playerPosition);
+
+        Random random = new Random();
+        int playerRollDie = random.nextInt(6) + 1;
+        System.out.println("Dice face : " + playerRollDie);
+        int rollDie = random.nextInt(3);
+        switch (rollDie) {
+            case 0:
+                playerPosition = playerPosition + playerRollDie;
+                break;
+            case 1:
+                playerPosition = playerPosition - playerRollDie;
+                break;
+            case 2:
+                playerPosition = playerPosition;
+                break;
+            default:
+                break;
+        }
+        System.out.println("Player is at : " + playerPosition);
     }
 }
